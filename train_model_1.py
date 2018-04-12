8a8d1946030361923cfb00c74b44661d045c2c9f
Ruksi <me@ruk.si>
2018-04-12 14:49:03 +0300

    Parametrize train_model_1.py

--- train_model_1.py
+++ train_model_1.py
@@ -1,3 +1,6 @@
+import os
+import glob
+import argparse
 import numpy as np
 import pandas as pd
 from keras.layers.core import Dense, Dropout
@@ -6,7 +9,11 @@ from keras.models import Sequential
 
 
 def get_data():
-    my_data = pd.read_csv('rte.csv', header=1, error_bad_lines=False)
+    # use the csv first file in the dataset directory
+    filename = glob.glob(os.path.join(settings.dataset_dir, "*.csv"))[0]
+    dataset_path = os.path.join(settings.dataset_dir, filename)
+
+    my_data = pd.read_csv(dataset_path, header=1, error_bad_lines=False)
     df = pd.DataFrame(my_data)
     df.drop(df.columns[[0, 3, 7, 8]], axis=1, inplace=True)
     return df
@@ -29,29 +36,7 @@ def load_data(my_data, seq_len):
     y_test = result[int(row):, -1][:, -1]
     x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], amount_of_features))
     x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], amount_of_features))
-    print(x_train[:3])
-    print(y_train[:3])
     return [x_train, y_train, x_test, y_test]
-
-
-df = get_data()
-values = df.values
-minima = np.amin(values[:, -1])
-maxima = np.amax(values[:, -1])
-scaling_parameter = maxima - minima
-values[:, 4] = (values[:, 4] - minima) / scaling_parameter
-values[:, 0] = (values[:, 0] - np.amin(values[:, 0])) / (np.amax(values[:, 0]) - np.amin(values[:, 0]))
-values[:, 1] = (values[:, 1] - np.amin(values[:, 1])) / (np.amax(values[:, 1]) - np.amin(values[:, 1]))
-values[:, 2] = (values[:, 2] - np.amin(values[:, 2])) / (np.amax(values[:, 2]) - np.amin(values[:, 2]))
-values[:, 3] = (values[:, 3] - np.amin(values[:, 3])) / (np.amax(values[:, 3]) - np.amin(values[:, 3]))
-
-df = pd.DataFrame(values)
-window = 5
-X_train, y_train, X_test, y_test = load_data(df[::-1], window)
-print("X_train", X_train.shape)
-print("y_train", y_train.shape)
-print("X_test", X_test.shape)
-print("y_test", y_test.shape)
 
 
 def build_model(layers):
@@ -63,24 +48,55 @@ def build_model(layers):
     model.add(Dropout(d))
     model.add(Dense(16, activation="relu", kernel_initializer="uniform"))
     model.add(Dense(1, activation="relu", kernel_initializer="uniform"))
-    model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+    model.compile(loss="mse", optimizer="adam", metrics=["accuracy"])
     return model
 
 
-model = build_model([5, window, 1])
-model.fit(
-    X_train,
-    y_train,
-    batch_size=1024,
-    epochs=50,
-    validation_split=0.2,
-    verbose=2)
+def main(settings):
+    df = get_data()
+    values = df.values
+    minima = np.amin(values[:, -1])
+    maxima = np.amax(values[:, -1])
+    scaling_parameter = maxima - minima
+    values[:, 4] = (values[:, 4] - minima) / scaling_parameter
+    values[:, 0] = (values[:, 0] - np.amin(values[:, 0])) / (np.amax(values[:, 0]) - np.amin(values[:, 0]))
+    values[:, 1] = (values[:, 1] - np.amin(values[:, 1])) / (np.amax(values[:, 1]) - np.amin(values[:, 1]))
+    values[:, 2] = (values[:, 2] - np.amin(values[:, 2])) / (np.amax(values[:, 2]) - np.amin(values[:, 2]))
+    values[:, 3] = (values[:, 3] - np.amin(values[:, 3])) / (np.amax(values[:, 3]) - np.amin(values[:, 3]))
 
-# serialize model to JSON
-model_json = model.to_json()
-with open("model.json", "w") as json_file:
-    json_file.write(model_json)
+    df = pd.DataFrame(values)
+    window = 5
+    X_train, y_train, X_test, y_test = load_data(df[::-1], window)
+    print("X_train", X_train.shape)
+    print("y_train", y_train.shape)
+    print("X_test", X_test.shape)
+    print("y_test", y_test.shape)
 
-# serialize weights to HDF5
-model.save_weights("model.h5")
-print("Saved model to disk")
+    model = build_model([5, window, 1])
+    model.fit(
+        X_train,
+        y_train,
+        batch_size=settings.batch_size,
+        epochs=settings.epochs,
+        validation_split=settings.validation_split,
+        verbose=2)
+
+    # serialize model to JSON
+    model_json = model.to_json()
+    with open(os.path.join(settings.output_dir, "model-layout.json"), "w") as json_file:
+        json_file.write(model_json)
+
+    # serialize weights to HDF5
+    model.save_weights(os.path.join(settings.output_dir, "model-weights.h5"))
+    print("Saved model to disk")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--epochs", type=int, required=True)
+    parser.add_argument("--batch_size", type=int, required=True)
+    parser.add_argument("--validation_split", type=float, required=True)
+    parser.add_argument("--dataset_dir", type=str, default="/valohai/inputs/dataset")
+    parser.add_argument("--output_dir", type=str, default="/valohai/outputs")
+    settings, unparsed = parser.parse_known_args()
+    main(settings)
