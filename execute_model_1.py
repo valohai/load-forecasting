333128521134f3eb1e6058f07c1c9671cfb7a416
Ali Farooq <alifarooq96@gmail.com>
2018-04-15 13:55:11 +0500

    refine execution of model 1

--- execute_model_1.py
+++ execute_model_1.py
@@ -13,13 +13,31 @@ from keras.models import model_from_json
 from keras.layers.core import Dense, Dropout, Activation
 from keras.layers.recurrent import LSTM
 from sklearn.metrics import mean_absolute_error
+import argparse
 
-def get_data(normalized=0):
-    my_data = pd.read_csv('rte.csv', header=1, error_bad_lines=False)
+def mean_absolute_percentage_error(y_true, y_pred):
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+def get_data(dataset_dir):
+    # use the csv first file in the dataset directory
+    dataset_path = glob.glob(os.path.join(dataset_dir, "*.csv"))[0]
+    
+    my_data = pd.read_csv(dataset_path, error_bad_lines=False)
     df = pd.DataFrame(my_data)
-    df.drop(df.columns[[0, 3, 7, 8]], axis=1, inplace=True)
-    #     df.drop(df.index[1000:])
-    return df
+    
+    column_names = list(df)
+    if 'Demand' in column_names:
+        # RTE dataset format
+        df = df.filter(items=['Day', 'Month', 'Hours', 'Temperature', 'Demand'])
+        return df, 'rte'
+    elif 'SYSLoad' in column_names:
+        # ERCOT dataset format.
+        df = df.filter(items=['Day', 'Month', 'Minutes' ,'SYSLoad'])
+        return df, 'ercot'
+    else:
+        raise Exception('Unknown dataset format with columns: {}'.format(column_names))
+
 
 def load_data(my_data, seq_len):
     amount_of_features = len(my_data.columns)
@@ -33,52 +51,72 @@ def load_data(my_data, seq_len):
     row = round(0.8 * result.shape[0])
     train = result[:int(row), :]
     x_train = train[:, :-1]
-    y_train = train[:, -1][:,-1]
+    y_train = train[:, -1][:, -1]
     x_test = result[int(row):, :-1]
-    y_test = result[int(row):, -1][:,-1]
+    y_test = result[int(row):, -1][:, -1]
     x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], amount_of_features))
     x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], amount_of_features))
-    print(x_train[:3])
-    print(y_train[:3])
     return [x_train, y_train, x_test, y_test]
 
-df = get_data(0)
-# min_max_scaler = preprocessing.MinMaxScaler()
-# nomalized = min_max_scaler.fit_transform(df.values)
-values = df.values
-minima = np.amin(values[:, -1])
-maxima = np.amax(values[:, -1])
-scaling_parameter = maxima - minima
-values[:, 4] = (values[:, 4]-minima)/scaling_parameter
-values[:, 0] = (values[:, 0]-np.amin(values[:, 0]))/(np.amax(values[:, 0])-np.amin(values[:, 0]))
-values[:, 1] = (values[:, 1]-np.amin(values[:, 1]))/(np.amax(values[:, 1])-np.amin(values[:, 1]))
-values[:, 2] = (values[:, 2]-np.amin(values[:, 2]))/(np.amax(values[:, 2])-np.amin(values[:, 2]))
-values[:, 3] = (values[:, 3]-np.amin(values[:, 3]))/(np.amax(values[:, 3])-np.amin(values[:, 3]))
+def main(settings):
+    df, dataset_format = get_data(settings.dataset_dir)
+    
+    values = df.values
+    minima = np.amin(values[:, -1])
+    maxima = np.amax(values[:, -1])
+    scaling_parameter = maxima - minima
+    if dataset_format == 'rte':
+        values[:, 0] = (values[:, 0] - np.amin(values[:, 0])) / (np.amax(values[:, 0]) - np.amin(values[:, 0]))
+        values[:, 1] = (values[:, 1] - np.amin(values[:, 1])) / (np.amax(values[:, 1]) - np.amin(values[:, 1]))
+        values[:, 2] = (values[:, 2] - np.amin(values[:, 2])) / (np.amax(values[:, 2]) - np.amin(values[:, 2]))
+        values[:, 3] = (values[:, 3] - np.amin(values[:, 3])) / (np.amax(values[:, 3]) - np.amin(values[:, 3]))
+        values[:, 4] = (values[:, 4] - minima) / scaling_parameter
+    elif dataset_format == 'ercot':
+        # TODO: do something with the Date column (values[:, 0])
+        values[:, 0] = (values[:, 0] - np.amin(values[:, 0])) / (np.amax(values[:, 0]) - np.amin(values[:, 0]))
+        values[:, 1] = (values[:, 1] - np.amin(values[:, 1])) / (np.amax(values[:, 1]) - np.amin(values[:, 1]))
+        values[:, 2] = (values[:, 2] - np.amin(values[:, 2])) / (np.amax(values[:, 2]) - np.amin(values[:, 2]))
+        values[:, 3] = (values[:, 3] - minima) / scaling_parameter
+    
+    df = pd.DataFrame(values)
+    window = 5
+    X_train, y_train, X_test, y_test = load_data(df[::-1], window)
+    print("X_train", X_train.shape)
+    print("y_train", y_train.shape)
+    print("X_test", X_test.shape)
+    print("y_test", y_test.shape)
 
-df = pd.DataFrame(values)
-window = 5
-X_train, y_train, X_test, y_test = load_data(df[::-1], window)
+    # load json and create model
+    json_file = open(glob.glob(os.path.join(settings.output_dir, "*.json"))[0], 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    model = model_from_json(loaded_model_json)
+    # load weights into new model
+    model.load_weights(glob.glob(os.path.join(settings.output_dir, "*.h5"))[0])
+    print("Loaded model from disk")
 
-# load json and create model
-json_file = open('model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-model = model_from_json(loaded_model_json)
-# load weights into new model
-model.load_weights("model.h5")
-print("Loaded model from disk")
+    predicted2 = model.predict(X_test)
+    actual = y_test
+    predicted2 = (predicted2*scaling_parameter)+minima
+    actual = (actual*scaling_parameter)+minima
 
-def mean_absolute_percentage_error(y_true, y_pred):
-    y_true, y_pred = np.array(y_true), np.array(y_pred)
-    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+    mape2 = sqrt(mean_absolute_percentage_error(predicted2, actual))
+    mse2  = mean_absolute_error(actual, predicted2)
+        
+    print(json.dumps({
+          "mape":mape2,
+          "mse":mse2
+          }))
 
-predicted2 = model.predict(X_test)
-actual = y_test
-predicted2 = (predicted2*scaling_parameter)+minima
-actual = (actual*scaling_parameter)+minima
 
-mape2 = sqrt(mean_absolute_percentage_error(predicted2, actual))
-print('Test MAPE2: %.3f' % mape2)
 
-mse2  = mean_absolute_error(actual, predicted2)
-print('Test MSE2: %.3f' % mse2)
+def cli():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset_dir", type=str, default="/valohai/inputs/dataset")
+    parser.add_argument("--output_dir", type=str, default="/valohai/outputs")
+    settings = parser.parse_args()
+    main(settings)
+
+
+if __name__ == "__main__":
+    cli()
